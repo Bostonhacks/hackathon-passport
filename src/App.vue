ac2803c16f055c295c36f3bf98e7adf609cea1dc
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-10-19 12:19:03 -0400

    add fonts and title, dropdown isnt working yet

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CreateEvent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CreateEvent from './components/CreateEvent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreateEvent
   }
 }
 </script>
---
+++ src/components/CreateEvent.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- <div id="createEvent" v-bind:style="{ backgroundColor:'#FFF0C8'}"> -->
+  <h1>Create New Event</h1>
+  <p>Event Type:</p>
+  <p>Event Name:</p>
+  <Dropdown
+    :options="[{ id: 1, name: 'Option 1'}, { id: 2, name: 'Option 2'}]"
+    :disabled="false"
+    name="zipcode"
+    :maxItem="10"
+    placeholder="Please select an option">
+  </Dropdown>
+</template>
+
+<script>
+import Dropdown from 'vue-simple-search-dropdown';
+export default {
+  name: 'CreateEvent',
+  beforeCreate () {
+    document.querySelector('body').setAttribute('style', 'background:#FFF0C8')
+  },
+  beforeUnmount () {
+    document.querySelector('body').setAttribute('style', '')
+  },
+  components: {
+      Dropdown
+  }
+}
+// document.body.style.backgroundColor = 'FFF0C8';
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+h1 {
+  font-family:'Roboto', sans-serif;
+  /* font-weight:300; */
+  font-size:3em;
+  color:#EC602D;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: black;
+}
+p {
+  font-family:'Roboto', sans-serif;
+}
+</style>
